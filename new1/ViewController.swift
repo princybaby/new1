a9361ab014dea1a9aa5d964e7bff86befaa9fe76
Docomo <Don@dons-macbook-pro.local>
2016-08-11 10:55:16 +0530

    Initial Commit

---
+++ new1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new1
+//
+//  Created by Don Dominic Mathew on 11/08/16.
+//  Copyright © 2016 Jesusdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
